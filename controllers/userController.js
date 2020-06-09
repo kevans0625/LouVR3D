67ee27eee90db09c955163c22bbfc3454e522c51
Kristin Evans <kevans0625@gmail.com>
2020-06-09 14:55:50 -0500

    added verification to the sign in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,21 @@
 const db = require("../models");
 
 module.exports = {
-    create: function(req, res) {
+    create: async (req, res) => {
+        const { username, email, password } = req.body;
+     
+        if (!username || !email || !password) {
+          return res.send({
+            success: false,
+            message: 'Error: All fields must be completed.'
+          });
+        }
+        const existingUser =  await db.User.findOne({email: email})
+        console.log(existingUser)
+        if (!existingUser == null){
+            return res.status(402).json({msg: "An account with this email address already exist."})
+        }
+         
         db.User
         .create({
             username: req.body.username,
@@ -11,18 +25,17 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(function(err) {
             if (err.name == 'ValidationError') {
-                console.error('Error Validating!', err);
+                console.error('Error Validating!');
                 // res.send('Error Validating!').json(err);
-                res.status(422).json(err);
+                res.status(422).json({message: err});
             } else {
-                console.error('Errorssssss Validating!', err);
-                res.status(500).json(err);
+                console.error('Validating!', err);
+                res.status(500).json({ error: err.message
+                });
             }
         })
-},
-    //     .catch(err => {console.log(err) 
-    //         res.status(422).json(err)})
-    // },
+    
+},        
     findAll: function(req, res) {
           db.User
             .find(req.query)
