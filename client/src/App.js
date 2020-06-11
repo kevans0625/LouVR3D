db93d38c893c19714d6ce1d12083549a16c820e7
Kristin Evans <kevans0625@gmail.com>
2020-06-10 21:56:20 -0500

    set up auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+  
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home/home';
@@ -7,10 +8,48 @@ import Signup from './pages/Signup/signup';
 import Profile from './pages/Profile/profile';
 import Exhibit from './pages/Exhibit/exhibit';
 import Favorites from './pages/Favorites/favorites';
+import UserContext from "../src/content/UserContext";
+import Axios from "axios";
 
 function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:3000/api/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      console.log(tokenRes.config.data)
+      if (tokenRes.config.data) {
+        const userRes = await Axios.get("http://localhost:3000/api/validUser", 
+        {
+          headers: { "x-auth-token": token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+        console.log(userRes)
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
   return (
     <Router>
+      <UserContext.Provider value={{ userData, setUserData }}>
+
     <div className="App">
       <Switch>
       {/* <Route exact path={["/", "/users"]}/> */}
@@ -24,6 +63,7 @@ function App() {
       </Switch>
       
     </div>
+      </UserContext.Provider>
     </Router>
   );
 }
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
 const imagesRoutes = require("./images");
-const exhibitRoutes = require("./exhibits")
+const exhibitRoutes = require("./exhibit")
 // const User = require("../../controllers/userController")
 const db = require("../../models");
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-
-const exhibitRoutes = require("./exhibit")
+const auth = require("../../middelware/auth")
 
 router.use("/user", userRoutes);
 
@@ -18,7 +17,6 @@ router.use("/exhibit", exhibitRoutes)
 router.post("/login", async function(req, res) {
   try {
     const {username, password } = req.body;
-
     // validate
     if (!username || !password)
       return res.status(400).json({ msg: "Not all fields have been entered." });
@@ -37,16 +35,39 @@ router.post("/login", async function(req, res) {
       token,
       user: {
         id: user._id,
-        displayName: user.displayName,
+        displayName: user.username,
       },
     });
-    console.log(token)
+    // console.log(token)
   } catch (err) {
     res.status(500).json({ error: err.message });
     console.log(err.message)
   }
 });
-        // });
-router.use("/exhibit", exhibitRoutes);
+
+router.post("/tokenIsVerified", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+
+    const user = await db.User.findById(verified.id);
+    if (!user) return res.json(false);
+
+    return res.json(true);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get("/validUser", auth, async (req, res) => {
+  const user = await db.User.findById(req.user);
+  res.json({
+    username: user.username,
+    id: user._id,
+  });
+});
 
 module.exports = router;
